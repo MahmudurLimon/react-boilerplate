af7f06b5690017925fca4d989e3d3a310927a366
MahmudurLimon <mahmudurlimon76@gmail.com>
2026-02-22 16:03:09 +0600

    new

--- src/UserAPI2.jsx
+++ src/UserAPI2.jsx
@@ -3,6 +3,12 @@ import { useState,useEffect } from "react";
 function UserAPI2() {
     const [users,setUsers] = useState([]);
     const [loading,setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [selectedUser,setSelectedUser] = useState(null);
+
+    //fetch users using promise
+    useEffect()
+
     return(
         <>
 
